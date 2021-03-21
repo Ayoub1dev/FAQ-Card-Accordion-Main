a438bf22edadc07bcd1d17ac35a3d786b5a86866
Ayoub1dev <enasa6608@gmail.com>
2021-03-21 19:06:26 +0000

    .

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ let ans = document.querySelectorAll('.answer');
 for(let i = 0; i < acc.length; i++){
     acc[i].addEventListener('click',function(){
         
-        this.classList.toggle("active");
-        this.classList.toggle('arrow-down')
+       
+        acc[i].classList.toggle("active");
+        acc[i].classList.toggle('arrow-down')
         
         if(ans[i].style.display === 'block'){
             ans[i].style.display = 'none';
         }else{
             ans[i].style.display = 'block';
         }
+         
     })
 }
