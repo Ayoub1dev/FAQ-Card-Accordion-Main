39bf4ef461161462458d52b910f2c84ddfaf5add
Ayoub1dev <enasa6608@gmail.com>
2021-03-21 18:23:01 +0000

    some hover effects when FAQ is hovered and js to turn the arrow 180deg

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-// get the question (element) and save it into acc variable so when the 
-// question is clicked the answer drops down
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = getElementsByClassName("answer");
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
 
 let acc = document.querySelectorAll('.question');
+let ans = document.querySelectorAll('.answer');
 
 for(let i = 0; i < acc.length; i++){
     acc[i].addEventListener('click',function(){
+        
         this.classList.toggle("active");
-        let ans = document.querySelectorAll('.answer');
-        console.log('dfs')
+        this.classList.toggle('arrow-down')
+        
         if(ans[i].style.display === 'block'){
             ans[i].style.display = 'none';
         }else{
